d780baf2db162e4e311f4b356a581777f26dfc3f
Tolulope Folorunso <tolufolorunso@yahoo.com>
2020-08-01 16:34:07 +0100

    Updated app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-const app = require("./controller.js");
+const app = require('./controller.js');
 
 app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+	console.log(`Server started on port ${PORT}`);
 });
